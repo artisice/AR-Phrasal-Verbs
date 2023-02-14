0b03d8403213c7d23e724bc67988adfd8f642f31
articise <arthur-lebedev@mail.ru>
2023-02-14 19:37:09 +0300

    first commit

---
+++ app/src/main/java/com/example/phrasalverbsar/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.phrasalverbsar;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static AnchorNode anchorNode;
+    private static boolean ModelFlag;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        ModelFlag = false;
+
+        // Инициализация Firebase Cloud Storage
+
+        FirebaseApp.initializeApp(this);
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        // Объявление AR фрагмента
+
+        ArFragment arFragment = (ArFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.arFragment);
+
+        FloatingActionButton fab = findViewById(R.id.fabHideFab);
+
+        // Объявление кнопки - перехода на Second Activity с ListView
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageButton listButton = findViewById(R.id.listButton);
+        listButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+            fab.show();
+        });
+
+        // обработка нажатия Floating Action Button
+        // Создание кеш файла и получение файла с облака
+
+                fab.setOnClickListener(v -> {
+                    if (!Objects.equals(SecondActivity.CurrentVerb, "")) {
+                        try {
+                            StorageReference modelRef = storage.getReference().child(SecondActivity.CurrentVerb);
+                            File file = File.createTempFile(SecondActivity.CurrentName, "glb");
+
+                            modelRef.getFile(file).addOnSuccessListener(taskSnapshot -> buildModel(file));
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        fab.hide();
+                    }
+                    else
+                        Toast.makeText(this, "Выбери глагол", Toast.LENGTH_SHORT).show();
+                });
+
+        // Обработка нажатия на метку плоской поверхности
+        // Создание якоря с моделью после рендеринга
+        // Анимация модели
+        // Добавление возможности крутить и масштабировать модель на сцене
+
+        if (arFragment != null) {
+            (arFragment).setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+                    if (ModelFlag) {
+                        if (fab.isOrWillBeHidden()) {
+                            anchorNode = new AnchorNode(hitResult.createAnchor());
+                            anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                            TransformableNode model = new TransformableNode(arFragment.getTransformationSystem());
+                            model.setParent(anchorNode);
+                            model.setRenderable(this.renderable).animate(true).start();
+                            model.select();
+                            ModelFlag = false;
+                        }
+                    }
+            });
+        }
+
+    }
+
+    // Переменная где будет хранится модель после рендеринга
+    private ModelRenderable renderable;
+
+    // Построение, рендеринг полученной модели с облака
+    private void buildModel(File file) {
+
+        ModelRenderable
+                .builder()
+                .setSource(this, Uri.parse(file.getPath()))
+                .setIsFilamentGltf(true)
+                .setAsyncLoadEnabled(true)
+                .setRegistryId(file.getPath())
+                .build()
+                .thenAccept(modelRenderable -> {
+                    Toast.makeText(this, "Модель построена", Toast.LENGTH_SHORT).show();
+                    renderable = modelRenderable;
+                    ModelFlag = true;
+
+                });
+    }
+
+}
---
+++ app/src/main/java/com/example/phrasalverbsar/SecondActivity.java
@@ -0,0 +1,100 @@
+package com.example.phrasalverbsar;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class SecondActivity extends AppCompatActivity {
+
+    // переменные, которые будут содержать данные о выбранном глаголе для построения модели
+    public static String CurrentVerb = "";
+    public static String CurrentName = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Phrasal verb list");
+
+        // Объявление списка ListView, который содержит данные объекта Verbs из Firebase realtime database
+
+        ListView VerbsList = findViewById(R.id.VerbList);
+        final ArrayList<String> list = new ArrayList<>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list);
+        VerbsList.setAdapter(adapter);
+
+        // Получение данных для списка глаголов из базы данных
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Verbs");
+        ref.addValueEventListener(new ValueEventListener() {
+
+        // Добавление полученных элементов в список
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    list.add(snapshot.getValue().toString());
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        // Обработка нажатия на элементы списка
+        // Получение данных о глаголе из базы данных по позиции в списке (название файла модели)
+
+        VerbsList.setOnItemClickListener((parent, v, position, id) -> {
+
+            DatabaseReference VRef = FirebaseDatabase.getInstance().getReference().child("CurrentVerb").child(String.valueOf(position));
+            DatabaseReference NRef = FirebaseDatabase.getInstance().getReference().child("CurrentName").child(String.valueOf(position));
+
+            VRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    CurrentVerb = dataSnapshot.getValue(String.class);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                }
+            });
+
+            NRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    CurrentName = dataSnapshot.getValue(String.class);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                }
+            });
+
+            if (MainActivity.anchorNode != null) {
+                MainActivity.anchorNode.setParent(null);
+            }
+            this.finish();
+        });
+    }
+
+}
